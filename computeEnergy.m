1cf3e801515b42e31904c7ca80437274299e3b11
Régis Cottereau <cottereau@lma.cnrs-mrs.fr>
2023-01-16 16:17:05 +0100

    [DEBUG] definitions of bins in time and space

--- computeEnergy.m
+++ computeEnergy.m
@@ -1,15 +1,5 @@
 function obs = computeEnergy(acoustics,obs,N,ip,it)
 
-% discretization in time
-dt = diff(obs.t);
-xt = [obs.t(1)-dt(1)/2 obs.t(1:end-1)+(dt/2) obs.t(end)+dt(end)/2];
-Nt = length(obs.t);
-
-% discretization in space
-dd = diff(obs.d);
-xd = [obs.d(1)-dd(1)/2 obs.d(1:end-1)+(dd/2) obs.d(end)+dd(end)/2];
-Nd = length(obs.d);
-
 % remove any non-necessary time steps
 obs.sensors = obs.sensors(1:it-1);
 % if ip==1 && obs.check
@@ -22,7 +12,7 @@ obs.sensors = cat(1,obs.sensors{:});
 % initialize energies
 Np = length(obs.rp);
 if ~isfield(obs,'energy')
-    obs.energy = zeros(Nd,Nt,Np);
+    obs.energy = zeros(length(obs.theta)/2,length(obs.t),Np);
 end
 
 % energy density as a function of [direction t rp]
@@ -36,7 +26,7 @@ if acoustics
         tmp(tmp(:,1)<-1,1) = -1;
         tmp(:,1) = acos(tmp(:,1));
         obs.energy(:,:,i1) = obs.energy(:,:,i1) ...
-                                  + histcounts2(tmp(:,1),tmp(:,2),xd,xt)/N;
+               + histcounts2(tmp(:,1),tmp(:,2),obs.binTheta,obs.binTime)/N;
     end
 else
     error('elasticity not implemented yet')
--- scatterDirections.m
+++ scatterDirections.m
@@ -1,14 +1,14 @@
 function scatterDirections(obs,station)
 figure;
 Nt = length(obs.t);
-theta = [-obs.d(end:-1:1) obs.d];
 for i1 = 1:length(station)
     v = VideoWriter(['scatterPlot' num2str(station(i1)) '.avi']);
     open(v);
     for i2=1:Nt
-        pol = [obs.energy(end:-1:1,i2,station(i1)); ...
-                                     obs.energy(:,i2,station(i1)) ];
-        polarplot(theta,pol);
+        pol = [obs.energy(:,i2,station(i1));
+               obs.energy(end:-1:1,i2,station(i1));
+               obs.energy(1,i2,station(i1))];
+        polarplot(obs.theta,pol);
         title(['time T = ' num2str(obs.t(i2)) 's'])
         M(i2) = getframe;
         writeVideo(v,M(i2));
