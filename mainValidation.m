7c30b6414ea17534d8856f794c51c852e4165e19
shahramkhazaee <shahramkhazaee@gmail.com>
2023-04-27 11:35:41 +0200

    Update mainValidation.m

--- mainValidation.m
+++ mainValidation.m
@@ -31,6 +31,9 @@ geometry = struct( 'type', 'fullspace', ...
                    'size', [4 3 3], ...
                    'dimension', 2 );
 
+% Plot the result on some particular points
+inds = [40 80 120]; % index of the desired observation points
+
 % radiative transfer solution
 obs = radiativeTransfer( source, material, observation, geometry );
 
@@ -38,17 +41,17 @@ obs = radiativeTransfer( source, material, observation, geometry );
 EP = Paasschens_RTE_Unbounded(source, material, observation, geometry);
 
 % Analytical check using Hoshiba's Monte Carlo-based approach (fixed obs point!)
-EH = Hoshiba_RTE_Unbounded_MonteCarlo(obs.t,obs.r(80),source,material,geometry,30);
+EH = Hoshiba_RTE_Unbounded_MonteCarlo(obs.t,obs.r(inds),source,material,geometry,10);
 
-% Plot the result on a particular point
-index = 80; % index of the desired observation point
 figure; hold on;
-plot(obs.t, obs.energyDensity(index,:)/(2*pi),'-k','LineWidth',2); % Why 2*pi is missing??!!
-plot(obs.t, EP(index,:),'-b');
-plot(obs.t,EH,'-r');
-grid on; box on; legend('Monte Carlo (our code)', 'Paasschens', 'Monte Carlo (Hoshiba)');
+h1 = plot(obs.t, obs.energyDensity(inds,:)/(2*pi),'-k'); % Why 2*pi is missing??!!
+h2 = plot(obs.t, EP(inds,:),'-b');
+h3 = plot(obs.t,EH,'-r');
+grid on; box on;
+legend( [h1(1), h2(1), h3(1)], {'Monte Carlo (our code)','Paasschens (analytical)', ...
+        'Monte Carlo (Hoshiba 1991)'},"Location",'best','FontSize',12);
 xlabel('Lapse Time [s]');
-ylabel(['Energy density at source-station distance of r = ', num2str(obs.r(index))])
+ylabel('Energy density at different source-station distances')
 
 
 %% Anisotropic scattering (to be done)
