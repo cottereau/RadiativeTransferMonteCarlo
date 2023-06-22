4355e8d81317563a8c18bae0c5a3e75d41aacc59
Régis Cottereau <cottereau@lma.cnrs-mrs.fr>
2023-06-22 11:08:49 +0200

    [DEBUG] Issue with coherent wave in Paaschens
    renormalized the Paasschens validation routine so as to obtain energies integrated over all angles for easier comparison with our code. Also, the coherent wave was not captured appropriately because x=ct was enforced too strongly. Modified to choose the x closest to ct in order to ensure that the coherent wave always appear in the simulation, whatever the space-time discretization

--- Paasschens_RTE_Unbounded.m
+++ Paasschens_RTE_Unbounded.m
@@ -13,22 +13,28 @@ function [E,E_diff] = Paasschens_RTE_Unbounded(source, material, observation, ge
 d = geometry.dimension;
 v = material.v;
 t = observation.time;
+Nt = length(t);
 
 Lmax = material.v*max(observation.time);
 [~,~,Rmax] = virtualSources( geometry, source.position, Lmax);
 r = linspace(0,Rmax,ceil(Rmax/observation.dr));
+Nr = length(r);
+dr = mean(diff(r));
 
-Sigma = prepareSigmaOne(material.sigma);
+Sigma = prepareSigmaOne(material.sigma); % homogeneous to 1/[L]
 meanFreeTime = 1/v/Sigma;
+D = v/2/Sigma;
 
 % a (normalized time)      : Sigma*v*t
 % b (normalized distance)  : r*Sigma, i.e. source-station distance * scattering cross-section
 a = t/meanFreeTime;
-b = r/v/meanFreeTime;
-% E : normalized energy density 
-    % in 2D : E : r*E/Sigma
-    % in 3D : E : r^2*E/Sigma
+b = r*Sigma;
+% E : energy density integrated over all angles 
+    % in 2D : E : 2*pi*r*E*dr
+    % in 3D : E : 4*pi*r^2*E*dr
 % E_diff : diffusion approximation (same normalization used here)
+
+% debug (Regis): I have not modified the E_diff after renormalization
 
 [m,n] = size(a);
 if m>n, a=a'; end
@@ -36,26 +42,27 @@ if m>n, a=a'; end
 if m<n, b=b'; end
 
 if d==2
-    E = b./(2*pi*sqrt(a.^2-b.^2)).*exp(sqrt(a.^2-b.^2)-a).*heaviside(a-b);
-    
-    aa = repmat(a,[length(b) 1]);
-    ind = (a==b);
-    if ~isempty(E(ind))
-        E(ind) = E(ind) + exp(-aa(ind))/(2*pi);
+    E = Sigma*dr*(b./a)./real(sqrt(1-(b./a).^2)).*exp(real(sqrt(a.^2-b.^2))-a);
+    E(heaviside(b-a)) = 0;
+    [~,ind] = min((a-b).^2,[],1);
+    for i1 = 1:Nt
+        E(ind(i1),i1) = E(ind(i1),i1) + exp(-a(i1));
     end
-
+    E(1,1) = 1;
     if nargout==2
-        E_diff = b*d.*exp(-d*b.^2./(4*a))./(4*pi*a);
+        E_diff = (dr*Sigma)*(b./a).*exp(-b.^2./(2*a));
     end
 elseif d==3
-    fun = @(x) exp(x).*sqrt(1+2.026./x);
-    E = b.^2.*real((1-(b./a).^2).^(1/8)).*exp(-a).*fun(a.*(1-(b./a).^2).^(3/4)).*heaviside(a-b)./(4*pi*a/3).^(3/2);
-    
-    aa = repmat(a,[length(b) 1]); bb = repmat(b,[1 length(a)]);
+    G = @(x) exp(x).*sqrt(1+2.026./x);
+    E = (4*pi)*b.^2.*real((1-(b./a).^2).^(1/8)).*exp(-a) ...
+        .*G(a.*(1-(b./a).^2).^(3/4)).*heaviside(a-b)./(4*pi*a/3).^(3/2);
+    aa = repmat(a,[Nr 1]);
     ind = (a==b);
     if ~isempty(E(ind))
-        E(ind) = E(ind) + exp(-aa(ind))./(4*pi*bb(ind));
+        E(ind) = E(ind) + exp(-aa(ind))/Sigma;
     end
+    E(:,1) = zeros(Nr,1);
+    E(1,1) = 1/Sigma;
 
     if nargout==2
         E_diff = b.^2.*exp(-d*b.^2./(4*a))./(4*pi*a/d).^(3/2);
@@ -64,10 +71,6 @@ else
     disp('Dimension "d" should be either 2 or 3 !')
 end
 
-E = E/(v*meanFreeTime)./r.^(d-1)';
-if nargout==2
-    E_diff = E_diff/(v*meanFreeTime)./r.^(d-1)';
-end
 end
 
 function h = heaviside(x)
--- mainValidation.m
+++ mainValidation.m
@@ -22,7 +22,7 @@ source      = struct( 'numberParticles', 1e6, ...
 material    = struct( 'acoustics', true, ...
                       'v', 1, ...
                       'sigma', @(th) 1/4/pi*ones(size(th)));         
-observation = struct( 'dr', 0.05, ...
+observation = struct( 'dr', 0.01, ...
                       'time', 0:0.1:15, ...
                       'Ndir', 100 );            
 geometry    = struct( 'type', 'fullspace', ...
@@ -31,12 +31,13 @@ geometry    = struct( 'type', 'fullspace', ...
 inds = [40 80 120]; % index of the desired observation points
 % running our code, Monte Carlo-based
 obs = radiativeTransfer( source, material, observation, geometry );
-Eus = obs.energyDensity/(2*pi);
+Eus = obs.energyDensity.*obs.dr';
 % computing Paasschens solution
 EP = Paasschens_RTE_Unbounded( source, material, observation, geometry );
 % running Hoshiba's Monte Carlo-based approach
 EH = Hoshiba_RTE_Unbounded_MonteCarlo( obs.t, obs.r(inds), ...
                                           source, material, geometry, 10 );
+EH = EH.*(2*pi*obs.dr(inds));
 % visual comparison
 figure; hold on; grid on; box on;
 h1 = plot( obs.t, Eus(inds,:), '-k' );
@@ -45,12 +46,11 @@ h3 = plot( obs.t, EH, '-r' );
 legend( [h1(1), h2(1), h3(1)], {'Monte Carlo (our code)','Analytical (Paasschens, 1997)', ...
         'Monte Carlo (Hoshiba 1991)'},'FontSize',12);
 xlabel('Lapse Time [s]');
-ylabel('Energy density at different source-station distances')
+ylabel('Integrated energy density at different source-station distances')
 title(titlecase);
 
 %% 3D Isotropic scattering (isotropic differential scattering cross-section)
-% input data
-titlecase = ['3D acoustic waves with isotropic differential scattering' ...
+% input datatitlecase = ['3D acoustic waves with isotropic differential scattering' ...
              ' cross section'];
 disp(['(2) Testing ' titlecase ' ...']);
 source      = struct( 'numberParticles', 1e6, ...
@@ -76,7 +76,7 @@ EP = Paasschens_RTE_Unbounded( source, material, observation, geometry );
 %                                           source, material, geometry, 10 );
 % visual comparison
 figure; hold on; grid on; box on;
-h1 = plot( obs.t, Eus(inds,:)/(4*pi), '-k' );
+h1 = plot( obs.t, Eus(inds,:), '-k' );
 h2 = plot( obs.t, EP(inds,:), '-b' );
 %h3 = plot( obs.t, EH, '-r' );
 legend( [h1(1), h2(1)], {'Monte Carlo (our code)','Analytical (Paasschens, 1997)'}, ...
