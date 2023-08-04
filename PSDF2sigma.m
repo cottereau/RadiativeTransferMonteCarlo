d287beed276f311c668ebadd2200cfa3f85b9c28
Régis Cottereau <cottereau@lma.cnrs-mrs.fr>
2023-08-04 18:41:16 +0200

    [CLEANUP] clarified the PSDF function and its input
    still some questions left in this routine, in particular concerning the impact of dimensionality on the sigma

--- PSDF2sigma.m
+++ PSDF2sigma.m
@@ -1,5 +1,18 @@
-function sigma = PSDF2sigma(acoustics, material, randpars)
-% This function calculates the normalized diffrential scatteing cross-sections
+function sigma = PSDF2sigma( d, material )
+% Compute the normalized differential scattering cross-sections based on
+% statistics of the fluctuating material parameters of the wave equation
+%
+% d                            dimension of the problem
+% material.acoustics           acoustics [true] or elastics [false]
+% material.Frequency           readial frequency
+% material.correlationLength   correlation length (assumed identical for
+%                              all material parameters)
+% material.spectralType        type of power spectrum density, assumed 
+%                              identical for all material parameters ('exp',
+%                              'power_law', 'gaussian', 'triangular' or 
+%                              'low_pass'
+% material.correlationMatrix   matrix giving the variance and correlation
+%                              between material parameters
 
 % Note 1: sigma has the unit 1/[T], as such, here the diff. scat. cross-sections
 % are normalized by the angular frequency w.
@@ -19,64 +32,66 @@ function sigma = PSDF2sigma(acoustics, material, randpars)
 % 2) See how the multiplication by the identity or other matrices should be
 % done in the code (it is not done everywhere).
 
+% constants
+acoustics = material.acoustics;
+zeta = material.Frequency/material.v*material.correlationLength;
+C = material.correlationMatrix;
+
 % test the symmetry of the correlation matrix
-if ~issymmetric(randpars.corr_matrix)
-    disp('corr_matirx field should be a symmetric matrix !')
+if ~issymmetric(material.correlationMatrix)
+    disp('correlationMatrix should be a symmetric matrix !')
 end
 
-psdf = randpars.PSDF_type;
-% The following normalized PSDF kernels are taken from Khazaie et al 2016 
-if strcmpi(psdf,'exp')
-    S = @(z) 1./(8*pi^2*(1+z.^2/4).^2);
-elseif strcmpi(psdf,'power_law')
-    S = @(z) 1./(pi^4)*exp(-2*z/pi);
-elseif strcmpi(psdf,'gaussian')
-    S = @(z) 1./(8*pi^3)*exp(-z.^2/4/pi);
-elseif strcmpi(psdf,'triangular')
-    S = @(z) (3/8/pi^4)*(1-z/2/pi).*heaviside(2*pi-z);
-elseif strcmpi(psdf,'low_pass')
-    S = @(z) (2/9/pi^4)*heaviside(3*pi/2-z);
-else
-    disp("PSDF should be 'exp','power_law','gaussian','triangular' or 'low_pass'")
+% The following normalized PSDF kernels are taken from Khazaie et al 2016
+% warning: these are for 3D: formulas should depend on dimensionality
+switch material.spectralType
+    case 'exp'
+        S = @(z) 1./(8*pi^2*(1+z.^2/4).^2);
+    case 'power_law'
+        S = @(z) 1./(pi^4)*exp(-2*z/pi);
+    case 'gaussian'
+        S = @(z) 1./(8*pi^3)*exp(-z.^2/4/pi);
+    case 'triangular'
+        S = @(z) (3/8/pi^4)*(1-z/2/pi).*heaviside(2*pi-z);
+    case 'low_pass'
+        S = @(z) (2/9/pi^4)*heaviside(3*pi/2-z);
+    otherwise
+        disp(['Power spectrum density (material.spectralType) should be' ...
+     ' ''exp'',''power_law'',''gaussian'',''triangular'' or ''low_pass'''])
 end
-
-d = geometry.dimension;
-% Normalized frequency
-zeta = randpars.normfreq;
 
 % Excep for sigmaSS, the constants in 2D & 3D cross sections are respectively 
 % pi/2 & pi^2. We can generalize in d-dimension using the following formula
 coeff = pi^(3*d/2-2)/2/gamma(d/2);
 
+% acoustics [Ryzhik et al, 1996 Eq. (1.3)]
 if acoustics 
-    std_kk = randpars.corr_matrix(1,1);
-    std_rr = randpars.corr_matrix(2,2);
-    std_kr = randpars.corr_matrix(1,2);
-       
-    % See Ryzhik et al 1996 Eq. (1.3);
+    std_kk = C(1,1); % variance of the compressibility
+    std_rr = C(2,2); % variance of the density
+    std_kr = C(1,2); % correlation of compressibility and density
     sigma = @(th) coeff*zeta^d*(cos(th).^2*std_rr^2 + 2*cos(th)*std_kr + std_kk^2) ...
-                  .*S(z.*sqrt(2*(1-cos(th)))).*sin(th);
-else
-    % Elastic
-    K = material.vp/material.vs;
-    std_ll = randpars.corr_matrix(1,1);
-    std_mm = randpars.corr_matrix(2,2);
-    std_rr = randpars.corr_matrix(3,3);
-    std_lm = randpars.corr_matrix(1,2);
-    std_lr = randpars.corr_matrix(1,3);
-    std_mr = randpars.corr_matrix(2,3);
+        .*S(zeta.*sqrt(2*(1-cos(th))));
 
+% elastics
+else
+    K = material.vp/material.vs;
     zetaP = zeta;
     zetaS = K*zetaP;
+    std_ll = C(1,1);
+    std_mm = C(2,2);
+    std_rr = C(3,3);
+    std_lm = C(1,2);
+    std_lr = C(1,3);
+    std_mr = C(2,3);
     
-    % See Ryzhik et al 1996 Eq. (4.55); and Turner 1998 Eq. (3)  
+    % [Ryzhik et al, 1996; Eq. (1.3)] and [Turner, 1998; Eq. (3)  
     sigmaPP = coeff*zetaP^d * ...
            ( (1-2/K^2)^2*std_ll^2 + 4*(1/K^2-2/K^4)*std_lm*cos(th).^2 + ...
              (4/K^4)*std_mm^2*cos(th).^2 + std_rr^2*cos(th).^2 + ...
              2*(1-2/K^2)*std_lr*cos(th) + (4/K^2)*std_mr*cos(th).^3 ) ...
              .*sin(th).^(d-2).*S(zetaP.*sqrt(2*(1-cos(th))));
 
-    % See Ryzhik 1996 Eq. (4.56) & Khazaie 2015 (PhD thesis) Eq. (1.81)
+    %  [Ryzhik, 1996; Eq. (4.56)] and [Khazaie, PhD thesis; Eq. (1.81)]
     sigmaPS = coeff*K*zetaP^d * ...
           ( K^2*std_rr^2 + 4*std_mm^2*cos(th).^2 + 4*K*std_mr*cos(th) )...
           .*sin(th).^(d-2).*S(zetaP.*sqrt(1+K^2-2*K*cos(th)));
@@ -112,4 +127,11 @@ else
 
     sigma = cell(sigmaPP,sigmaPS,sigmaSP,sigmaSS);
 
+end
+end
+
+function h = heaviside(h)
+h(h>0) = 1;
+h(h==0) = 1/2;
+h(h<0) = 0;
 end
