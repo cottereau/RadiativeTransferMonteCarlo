f4384ab952b5c0827151e64f4a2419e763e8a034
Régis Cottereau <cottereau@lma.cnrs-mrs.fr>
2023-08-04 19:06:14 +0200

    [DEBUG] there seems to be an error in the formulas for PSDF as sigma is sometimes negative

--- prepareSigmaOne.m
+++ prepareSigmaOne.m
@@ -5,17 +5,21 @@ Nth = 1000;
 % as input ...
 % sigma is defined rather a function of cos(\thet)=\xi instead of the scattering angle theta itself. 
 % Hence, the intergation will be done over (-1,+1).
+xth = linspace(0,pi,Nth);
+if any(sigma(xth)<0)
+    warning('sigma function should be positive. This does not seem to be the case')
+    sigma = @(th) max(0,sigma(th));
+end
 if d==2
     Sigma = 2*integral(sigma,0,pi);
     sigmaNorm = @(th) 2*sigma(th)/Sigma;
-    ind = 1:Nth;
 elseif d==3
     Sigma = 2*pi*integral(@(th)sigma(th).*sin(th),0,pi);
     sigmaNorm = @(th) sigma(th).*sin(th)/Sigma;
-    ind = [1 3:Nth-2 Nth];
 end
-xth = linspace(0,pi,Nth);
 pdf = sigmaNorm(xth);
 cdf = cumsum(pdf)*(pi/Nth);
+ind = [find(diff(cdf)>0) Nth];
+ind(1) = 1;
 invcdf = griddedInterpolant(cdf(ind),xth(ind));
 end
