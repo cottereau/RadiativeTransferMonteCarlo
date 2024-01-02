05e37d53f2a5f429a50df141d1e7469b7803922b
Régis Cottereau <cottereau@lma.cnrs-mrs.fr>
2024-01-02 15:27:25 +0100

    [CLEANUP] unnecessary complex notation

--- radiativeTransferUnbounded.m
+++ radiativeTransferUnbounded.m
@@ -66,7 +66,7 @@ parfor ip = 1:Np
 
 % end of loop on packages
 end
-obs.energyIncoherent = (1./(obs.dr'*obs.N)).*Ei(:,:,:,1:1+~acoustics);
+obs.energyIncoherent = (1./(obs.dr'*obs.N)).*Ei;
 obs.energyDomainCoherent = Ec(:,1:1+~acoustics)/obs.N;
 
 % energy density as a function of [x t] and [t]
