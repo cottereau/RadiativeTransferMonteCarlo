57931051c05ec40766cc5fcfe5cc5837b98a022c
shahramkhazaee <shahramkhazaee@gmail.com>
2024-01-10 23:40:28 +0100

    [DEBUG] issue with differential scattering cross sections

--- prepareSigmaOne.m
+++ prepareSigmaOne.m
@@ -1,15 +1,6 @@
 function [Sigma,invcdf] = prepareSigmaOne(sigma,d)
 Nth = 1000;
-% Question : why the dependence of sigma/Sigma on q/k is not considered?
-% lc (correlation distance) and the correlation model should also be given
-% as input ...
-% sigma is defined rather a function of cos(\thet)=\xi instead of the scattering angle theta itself. 
-% Hence, the intergation will be done over (-1,+1).
 xth = linspace(0,pi,Nth);
-if any(sigma(xth)<0)
-    warning('sigma function should be positive. This does not seem to be the case')
-    sigma = @(th) max(0,sigma(th));
-end
 if d==2
     Sigma = 2*integral(sigma,0,pi);
     sigmaNorm = @(th) (2/Sigma)*sigma(th);
