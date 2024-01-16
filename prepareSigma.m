a919d62387e25d313cb29db6d5a6fdab0bd8cbe2
shahramkhazaee <shahramkhazaee@gmail.com>
2024-01-16 17:22:32 +0100

    [UPDATE] adding diffusion coefficients

--- prepareSigma.m
+++ prepareSigma.m
@@ -6,7 +6,10 @@ end
 
 if mat.acoustics
     
-    [mat.Sigma,mat.invcdf] = prepareSigmaOne(mat.sigma,d);
+    [mat.Sigma,mat.Sigmapr,mat.invcdf] = prepareSigmaOne(mat.sigma,d);
+
+    % Diffusion coefficient m²/s (Eq. (5.12), Ryzhik et al, 1996)
+    mat.D = mat.v^2/(d*(mat.Sigma-mat.Sigmapr));
 
     mat.meanFreeTime = 1/mat.Sigma;
     % the two lines below are just for homogenization of the propagation
@@ -17,11 +20,25 @@ else
     mat.Sigma = zeros(2);
     mat.invcdf = cell(2);
 
-    [mat.Sigma(1,1),mat.invcdf{1,1}] = prepareSigmaOne(mat.sigma{1,1},d);
-    [mat.Sigma(1,2),mat.invcdf{1,2}] = prepareSigmaOne(mat.sigma{1,2},d);
-    [mat.Sigma(2,1),mat.invcdf{2,1}] = prepareSigmaOne(mat.sigma{2,1},d);
-    [mat.Sigma(2,2),mat.invcdf{2,2}] = prepareSigmaOne(mat.sigma{2,2},d);
+    [mat.Sigma(1,1),mat.Sigmapr(1,1),mat.invcdf{1,1}] = prepareSigmaOne(mat.sigma{1,1},d);
+    [mat.Sigma(1,2),mat.Sigmapr(1,2),mat.invcdf{1,2}] = prepareSigmaOne(mat.sigma{1,2},d);
+    [mat.Sigma(2,1),mat.Sigmapr(2,1),mat.invcdf{2,1}] = prepareSigmaOne(mat.sigma{2,1},d);
+    [mat.Sigma(2,2),mat.Sigmapr(2,2),mat.invcdf{2,2}] = prepareSigmaOne(mat.sigma{2,2},d);
     mat.meanFreeTime = 1./sum(mat.Sigma,2);
+    
+    
+    K = mat.vp/mat.vs;
+    % Transport mean free paths of P & S waves
+    tmfp_P = (mat.vp*(mat.Sigma(2,2)+mat.Sigma(2,1)-mat.Sigmapr(2,2)) + mat.vs*mat.Sigmapr(1,2) )...
+                      /( (mat.Sigma(1,1)+mat.Sigma(1,2)-mat.Sigmapr(1,1))*(mat.Sigma(2,2)+mat.Sigma(2,1)-mat.Sigmapr(2,2))- mat.Sigmapr(1,2)*mat.Sigmapr(2,1) );
+    tmfp_S = (mat.vs*(mat.Sigma(1,1)+mat.Sigma(1,2)-mat.Sigmapr(1,1)) + mat.vp*mat.Sigmapr(2,1) )...
+                      /( (mat.Sigma(1,1)+mat.Sigma(1,2)-mat.Sigmapr(1,1))*(mat.Sigma(2,2)+mat.Sigma(2,1)-mat.Sigmapr(2,2))- mat.Sigmapr(1,2)*mat.Sigmapr(2,1) );
+    % partial diffusion coefficients of P & S waves
+    Dp = mat.vp*tmfp_P/d; 
+    Ds = mat.vs*tmfp_S/d;
+    % Diffusion coefficient m²/s (Eqs. (5.42) & (5.46), Ryzhik et al, 1996)
+    mat.D = (Dp+2*K^3*Ds)/(1+2*K^3);
+
     mat.P2P = mat.Sigma(1,1)/sum(mat.Sigma(1,:),2); 
     mat.S2S = mat.Sigma(2,2)/sum(mat.Sigma(2,:),2);
 end
--- prepareSigmaOne.m
+++ prepareSigmaOne.m
@@ -1,11 +1,13 @@
-function [Sigma,invcdf] = prepareSigmaOne(sigma,d)
+function [Sigma,Sigma_prime,invcdf] = prepareSigmaOne(sigma,d)
 Nth = 1000;
 xth = linspace(0,pi,Nth);
 if d==2
     Sigma = 2*integral(sigma,0,pi);
+    Sigma_prime = 2*integral(@(th)sigma(th).*cos(th),0,pi);
     sigmaNorm = @(th) (2/Sigma)*sigma(th);
 elseif d==3
     Sigma = 2*pi*integral(@(th)sigma(th).*sin(th),0,pi);
+    Sigma_prime = 2*pi*integral(@(th)sigma(th).*sin(th).*cos(th),0,pi);
     sigmaNorm = @(th) (2*pi/Sigma)*sin(th).*sigma(th);
 end
 pdf = sigmaNorm(xth);
