77347ad8fd839ccfa88e7a2bb079dca7a776efa2
shahramkhazaee <shahramkhazaee@gmail.com>
2024-02-01 16:30:18 +0100

    [DEBUG] 2D elastic isotropic scattering case

--- mainValidation.m
+++ mainValidation.m
@@ -11,7 +11,7 @@
 % anisotropic scattering in particular)
 % 3D acoustic is currently under debug 
 
-%% 2D Isotropic scattering (isotropic differential scattering cross-section)
+%% 2D Isotropic scattering acoustic (isotropic differential scattering cross-section)
 % input data
 titlecase = ['2D acoustic waves with isotropic differential scattering' ...
              ' cross section'];
@@ -94,6 +94,55 @@ xlabel('Lapse Time [s]');
 ylabel('Integrated energy density at different source-station distances')
 title(titlecase);
 
+%% 2D Isotropic scattering elastic (Nakahara & Yoshimoto, 2011)
+titlecase = ['2D elastic waves with isotropic differential scattering' ...
+             ' cross sections'];
+% Load the results of the paper
+load('Nakahara_Yoshimoto_2011_Fig4_left.mat')
+
+% Run our code
+geometry = struct( 'dimension', 2 );
+
+source = struct( 'numberParticles', 1e6, ...
+                 'polarization', 'S', ...
+                 'lambda', 0.001 );
+
+observation = struct('dr', 0.4, ...        % size of bins in space
+                     'time', 0:0.0167:34.13, ...  % observation times
+                     'Ndir', 10 );         % number of bins for directions           
+ 
+% material properties
+% material.coefficients_of_variation defines the coefficients of variaiton
+% of lambda, mu (Lamé coefficients) and rho (density), respectively.
+% material.correlation_coefficients defines the correlation coefficient
+% between (lambda,mu), (lambda,rho), and (mu,rho), respectively.
+material = struct( 'acoustics', false, ...
+                   'vp', 6, ...
+                   'vs', 3.46);
+% Values taken from (Nakahara & Yoshimoto, 2011, Section 3.1)
+K = material.vp/material.vs;
+gpp = 0.05; gps = 0.05; gp = gpp + gps;
+gsp = gps/K; gss = (material.vp*gp - material.vs*gsp)/material.vs;
+material.sigma = {@(th) 1/2/pi*ones(size(th))*gpp*material.vp, @(th) 1/2/pi*ones(size(th))*gps*material.vp; ...
+                  @(th) 1/2/pi*ones(size(th))*gsp*material.vs, @(th) 1/2/pi*ones(size(th))*gss*material.vs};
+
+obsS = radiativeTransferUnbounded( geometry.dimension, source, material, observation );
+
+% Epc = obs.Ec(:,:,1); Epi = obs.Ei(:,:,1); Ep = Epc+Epi; Esc = obs.Ec(:,:,2); Esi = obs.Ei(:,:,2); Es = Esc+Esi;
+EpcP = obsP.Ec(:,:,1); EpiP = obsP.Ei(:,:,1); EpP = EpcP+EpiP; EscP = obsP.Ec(:,:,2); EsiP = obsP.Ei(:,:,2); EsP = EscP+EsiP;
+EpcS = obsS.Ec(:,:,1); EpiS = obsS.Ei(:,:,1); EpS = EpcS+EpiS; EscS = obsS.Ec(:,:,2); EsiS = obsS.Ei(:,:,2); EsS = EscS+EsiS;
+Ep = (1*EpP+1.5*K^5*EpS)/(1+1.5*K^5); Es = (1*EsP+1.5*K^5*EsS)/(1+1.5*K^5);
+% Normalized P-wave energy in terms of normalized time
+figure; semilogy(obs.t*gp*material.vp,2*pi*Ep(26,:)/(1/(1+1.5*K^5)*(gp*material.vp)^2),'-b','linewidth',2);
+xlabel('Normalized Time [-]'); ylabel('Normalized P-wave energy [-]');
+xlim([0 6]); ylim([0.001 100]); hold on; plot(energy_P(:,1),energy_P(:,2),'-r','linewidth',2);
+yticks([0.001 0.01 0.1 1 10 100]); yticklabels({'0.001','0.01','0.1','1','10','100'});
+
+% Normalized P-wave energy in terms of normalized time
+figure; semilogy(obs.t*gp*material.vp,2*pi*Es(26,:)/(1/(1+1.5*K^5)*(gp*material.vp)^2),'-b','linewidth',2); 
+xlabel('Normalized Time [-]'); ylabel('Normalized S-wave energy [-]'); 
+xlim([0 6]); ylim([0.001 100]); hold on; plot(energy_S(:,1),energy_S(:,2),'-r','linewidth',2);
+yticks([0.001 0.01 0.1 1 10 100]); yticklabels({'0.001','0.01','0.1','1','10','100'});
 
 %% 3D Anisotropic scattering (isotropic differential scattering cross-section)
 % to be done
