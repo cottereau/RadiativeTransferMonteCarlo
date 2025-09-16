bf3a9b9e49bc94c5d461b857f9252585cf8a8b3f
Régis Cottereau <cottereau@lma.cnrs-mrs.fr>
2025-09-16 13:57:00 +0200

    [DEBUG] in case first histogram is one unit

--- radiativeTransferUnbounded.m
+++ radiativeTransferUnbounded.m
@@ -49,4 +49,4 @@ end
 obs.energyDensity = (1./(d1'*(d2*obs.N))).*double(E);
 
 % energy as a function of [t]
-obs.energy = tensorprod(d1,squeeze(tensorprod(d2,obs.energyDensity,2,2)),2,1);
+obs.energy = tensorprod(d1,reshape(tensorprod(d2,obs.energyDensity,2,2),[length(d1) Nt]),2,1);
